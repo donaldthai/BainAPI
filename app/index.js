484c31cfdf4d09084707f82d417101ff472cb0bc
Donald Thai <donald.thai@gmail.com>
2018-07-05 22:20:44 -0700

    updated app

--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = (process.env.PORT || 5000)
 const providers = require('./providers')
-
 var info = "To get started, use the API: GET /providers?max_discharges=5&min_discharges=6&max_average_covered_charges=50000&min_average_covered_charges=40000&min_average_medicare_payments=6000&max_average_medicare_payments=10000&state=GA";
+
+app.set('port', port)
 
 app.get('/', (request, response) => {
   response.send(info);
